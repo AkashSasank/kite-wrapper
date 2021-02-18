403d7971ae2edb06a302fe4f9bc68153991d89f1
akashc <akashc@qburst.com>
2021-02-18 13:57:44 +0530

    Updated setup.

--- setup.py
+++ setup.py
@@ -7,14 +7,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(name='kite-wrapper',
       version='0.0.1',
       description='A wrapper for kiteconnect API.',
-      url='#',
+      url='https://github.com/AkashSasank/kite-wrapper',
       author='Akash Sasank',
       author_email='akashsasank369@gmail.com',
       license='MIT',
       packages=setuptools.find_packages(),
       classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
       ],
-      python_requires='>=3.6',)
+      python_requires='>=3.6',
+      install_requires=['kiteconnect', 'selenium==3.141.0', 'numpy==1.20.1', 'pandas==1.2.2', 'stockstats==0.3.2'], )
