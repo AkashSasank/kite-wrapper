95632921ef4b3a4b3b1daac289747ad6da71b342
akashc <akashc@qburst.com>
2021-03-13 13:42:21 +0530

    Updated version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='kite-wrapper',
-      version='0.0.2',
+      version='0.0.3',
       description='A wrapper for kiteconnect API.',
       url='https://github.com/AkashSasank/kite-wrapper',
       author='Akash Sasank',
